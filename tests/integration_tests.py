57f51db01f2799fed2b591e8840125e9d958fa63
Romain Sacchi <romain.sacchi@aalborgportland.com>
2019-04-11 18:12:52 +0200

    Update integration_tests.py

--- tests/integration_tests.py
+++ tests/integration_tests.py
@@ -9,18 +9,18 @@ class TestIntegrations(TestCase):
     
     # We check that a GET request on /activities/ with a "lim" parameter of 5 returns a JSON with 5 entries only.
     def test_activities_list_with_limit(self):
-        response = self.app.get('/activities/', query_string='lim=5')
+        response = self.app.get('/v1/activities/', query_string='lim=5')
         self.assertEqual(
             len(list(json.loads(response.get_data().decode(sys.getdefaultencoding())))),5)
         
     # We check that a GET request on /activities/ without a limit defined will return a JSON of length 100.
     def test_activities_list(self):
-        response = self.app.get('/activities/')
+        response = self.app.get('/v1/activities/')
         self.assertEqual(
             len(list(json.loads(response.get_data().decode(sys.getdefaultencoding())))),100)
         
     # We check that a GET request on /activities/ with an invalid "lim" parameter returns a useful error message.
     def test_activities_list_invalid_lim(self):
-        response = self.app.get('/activities/', query_string='lim=6d')
+        response = self.app.get('/v1/activities/', query_string='lim=6d')
         self.assertEqual(
-            json.loads(response.get_data().decode(sys.getdefaultencoding())),{"message": "The 'lim' parameter does not seem to be of type integer."})
+            json.loads(response.get_data().decode(sys.getdefaultencoding())),{'detail': "Wrong type, expected 'integer' for query parameter 'lim'",'status': 400,'title': 'Bad Request','type': 'about:blank'})
